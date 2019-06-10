9c8889fe83f2a69f447adfda0b70c4db4b3cf650
pumpkye <pumpkye@gmail.com>
2019-06-10 18:51:05 +0800

    移除编译目标文件夹

--- dist/message/MessagegBase.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class MessageBase {
-    constructor() {
-        this.data = {};
-    }
-}
-exports.MessageBase = MessageBase;
-//# sourceMappingURL=MessagegBase.js.map
--- dist/test/TestCardPool.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=TestCardPool.js.map
